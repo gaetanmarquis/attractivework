ec0e2b3cf2c227f8dc13233abe2b87352c2e8474
carolineARNOUX <arnoux.carolina@gmail.com>
2019-08-20 15:21:45 +0200

    creation form personnalite

--- src/Controller/PersonnaliteController.php
+++ src/Controller/PersonnaliteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Personnalite;
+use App\Form\PersonnaliteType;
 use App\Repository\PersonnaliteRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +20,36 @@ class PersonnaliteController extends AbstractController
         $personnalites = $personnaliteRepository->findAll();
         return $this->render('personnalite/index.html.twig', [
             'personnalites' => $personnalites,
+        ]);
+    }
+
+    /**
+     * @Route("/membre/add", name="personnalite_add")
+     */
+    public function add(Request $request, ObjectManager $objectManager)
+    {
+        //Adapter les 3 lignes ci-dessous selon la table en BDD
+        //Il s'agit de la création du formulaire
+        $personnalite = new Personnalite();
+        $personnaliteForm = $this->createForm(PersonnaliteType::class, $personnalite);
+        $personnaliteForm->handleRequest($request);
+
+        //A la soumission du formulaire
+        if( $personnaliteForm->isSubmitted() && $personnaliteForm->isValid() ){
+            //Pour les champs de type DateTime, utiliser le setter() avec comme argument new \DateTime
+            $personnalite->setDateInscription( new \DateTime() );
+
+            //Injection en BDD
+            $objectManager->persist($personnalite);
+            $objectManager->flush();
+
+            //Redirection vers l'affichage - Mettre en argument le nom de la route
+            return $this->redirectToRoute('personnalite');
+        }
+
+        //Rendu du formulaire
+        return $this->render('personnalite/add.html.twig', [
+            'personnalite_form' => $personnalite_form->createView(),
         ]);
     }
 }
