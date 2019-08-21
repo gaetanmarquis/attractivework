b0584df44280494950865c3dd4def731409090a4
jaypee97 <franckwest97@yahoo.fr>
2019-08-21 10:24:51 +0200

    like & match

---
+++ src/Form/LikeType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Candidat;
+use App\Entity\Like;
+use App\Entity\Recruteur;
+use Symfony\Component\Form\AbstractType;
+use PUGX\AutocompleterBundle\Form\Type\AutocompleteType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LikeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_like', DateType::class, [
+                'label'=>"Date du like"
+    ])
+            ->add('candidat', AutocompleteType::class, [
+                'class' => Candidat::class,
+                'label' => "id candidat"
+            ])
+            ->add('recruteur', AutocompleteType::class, [
+                'class' => Recruteur::class,
+                'label' => "id recruteur"
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=>"Ajouter un like"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Like::class,
+        ]);
+    }
+}
