82f0a295de3c523427d01284c557e191a91a08ed
carolineARNOUX <arnoux.carolina@gmail.com>
2019-08-27 16:03:10 +0200

    MAJ profilDetailsRecruteur recruteur font

--- src/Controller/ProfilDetailsRecruteurController.php
+++ src/Controller/ProfilDetailsRecruteurController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Recruteur;
+use App\Repository\RecruteurRepository;
+use App\Entity\Membre;
+use App\Repository\MembreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class ProfilDetailsRecruteurController extends AbstractController
 {
     /**
-     * @Route("/profil/details/recruteur", name="profil_details_recruteur")
+     * @Route("/profil/details/recruteur/{id}", name="profil_details_recruteur")
      */
-    public function index()
+    public function index( RecruteurRepository $recruteurRepository, int $id )
     {
+
+    	$recruteur = $recruteurRepository->createQueryBuilder('r')
+    	->where('r.id = :id')
+    	->setParameter('id', $id)
+    	->join('r.membre', 'm')
+    	->addSelect('m')
+        ->join('m.personnalite', 'p')
+        ->addSelect('p')
+    	->getQuery()
+    	->getResult();
+
         return $this->render('profil_details_recruteur/index.html.twig', [
-            'controller_name' => 'ProfilDetailsRecruteurController',
+            'recruteur' => $recruteur[0],
         ]);
     }
 }
