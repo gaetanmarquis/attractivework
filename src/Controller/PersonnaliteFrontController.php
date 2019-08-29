46d461937eaf6fb507c358aa8c080f5e0642763b
jaypee97 <franckwest97@yahoo.fr>
2019-08-29 15:30:52 +0200

    type_personnalites

--- src/Controller/PersonnaliteFrontController.php
+++ src/Controller/PersonnaliteFrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PersonnaliteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,12 @@ class PersonnaliteFrontController extends AbstractController
     /**
      * @Route("/types-de-personnalite", name="types-de-personnalite")
      */
-    public function index()
+    public function index( PersonnaliteRepository $personnaliteRepository )
     {
+
+        $personnalites = $personnaliteRepository->findAll();
         return $this->render('personnalite_front/index.html.twig', [
-            'controller_name' => 'PersonnaliteFrontController',
+            'personnalites' => $personnalites,
         ]);
     }
 }
